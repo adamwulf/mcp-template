dfad9490cbed0aae2bb4846e7a37b1321a52b29b
Adam Wulf <adam@adamwulf.me>
2025-03-16 22:30:57 -0500

    Basic mac app, includes the mcp-template package

---
+++ MCPExample/MCPExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MCPExample
+//
+//  Created by Adam Wulf on 3/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MCPExample/MCPExample/MCPExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCPExampleApp.swift
+//  MCPExample
+//
+//  Created by Adam Wulf on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MCPExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
