363b4c2247f590f1e9e30382ff4d542f101a3bd4
Adam Wulf <adam@adamwulf.me>
2025-03-30 13:54:33 -0500

    Updating to upstream mcp sdk repo

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url: "https://github.com/adamwulf/mcp-swift-sdk", branch: "feature/wait-until-complete")
+        .package(url: "https://github.com/modelcontextprotocol/swift-sdk", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -30,7 +30,7 @@ let package = Package(
         .target(
             name: "EasyMCP",
             dependencies: [
-                .product(name: "MCP", package: "mcp-swift-sdk")
+                .product(name: "MCP", package: "swift-sdk")
             ]),
         .executableTarget(
             name: "mcpexample",
